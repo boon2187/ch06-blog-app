b208455770317f619e83b2a68a92330f9dc7b35e
boon2187 <fumiya.mai.ito@gmail.com>
2025-03-13 23:35:36 +0900

    Articleコンポーネントがliタグで表示されるようにし、keyも設定した。

--- src/App.jsx
+++ src/App.jsx
@@ -17,12 +17,14 @@ function App() {
       <div className="h-14 w-full bg-gray-800">
         <Header />
       </div>
-      <div className="flex-1 w-full flex flex-col items-center mt-10">
-        {articles.map((article) => (
-          <div className="mb-5 w-1/2 p-4 bg-gray-800">
-            <Article article={article} />
-          </div>
-        ))}
+      <div className="mt-10">
+        <ul className="flex-1 w-full flex flex-col items-center">
+          {articles.map((article) => (
+            <li key={article.id} className="mb-5 w-1/2 p-4 bg-gray-800">
+              <Article article={article} />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
