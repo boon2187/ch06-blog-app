c86b3954323cda52daa59acfe7bb255b71261ca9
boon2187 <fumiya.mai.ito@gmail.com>
2025-03-13 21:54:35 +0900

    最初の描写時に、記事データをarticlesに保存。また、直接記事の数だけ、Articleコンポーネントを描写するようにしたので、Articlesコンポーネントが不要になった。

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import "./App.css";
-import Articles from "./components/Articles";
 import Header from "./components/Header";
+import { useState, useEffect } from "react";
+import { posts } from "./data/posts";
+import Article from "./components/Article";
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // 初期表示時にpostsデータを読み込む
+    setArticles(posts);
+  }, []);
+
   return (
     <>
       <Header />
-      <Articles />
+      {articles.map((article) => (
+        <Article key={article.id} article={article} />
+      ))}
     </>
   );
 }
