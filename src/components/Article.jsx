06d48a3eab86728d58f839c5a001cce3d1a78822
boon2187 <fumiya.mai.ito@gmail.com>
2025-03-24 21:43:15 +0900

    とりあえず、タイトル、ボタン、記事情報など、すべての文字が表示されるようにした

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
 export default function Article({ article }) {
-  return <div>{article.title}</div>;
+  return (
+    <div>
+      <div>{article.createdAt}</div>
+      <div>
+        {article.categories.map((category, index) => (
+          <span key={index}>{category}</span>
+        ))}
+      </div>
+      <h2>{article.title}</h2>
+      <div>{article.content}</div>
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between items-center px-10">
+      <h1>Blog</h1>
+      <button>お問い合わせ</button>
+    </div>
+  );
 }
