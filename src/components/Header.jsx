d26036f40d4b413d39c87ad35d4a65dad591163c
boon2187 <fumiya.mai.ito@gmail.com>
2025-03-25 22:36:49 +0900

    リンクなので、aタグに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-10 bg-gray-900 h-[72px]">
-      <h2 className="text-xl text-white">Blog</h2>
-      <button className="text-white bg-gray-900 mt-1">お問い合わせ</button>
+      <a className="text-xl text-white">Blog</a>
+      <button className="text-white bg-gray-900 mt-1 cursor-pointer">
+        お問い合わせ
+      </button>
     </header>
   );
 }
