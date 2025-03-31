2618e1d8e9a2d71929808cc9fa2372bb5cbfc8a2
boon2187 <fumiya.mai.ito@gmail.com>
2025-03-31 20:22:47 +0900

    本文表示を、dangerouslySetInnerHTMLに変更。

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,22 +1,6 @@
 import React from "react";
 import { format } from "date-fns";
 export default function Article({ post }) {
-  const formatContent = (content) => {
-    // HTMLタグを改行に変換
-    const textWithLineBreaks = content.replace(/<br\s*\/?>/g, "\n");
-    // HTMLタグを除去
-    const plainText = textWithLineBreaks.replace(/<[^>]*>/g, "");
-    // 60文字で切り詰め
-    const truncatedText = plainText.slice(0, 60);
-    // 改行を<br/>タグに戻す
-    return truncatedText.split("\n").map((line, index) => (
-      <React.Fragment key={index}>
-        {line}
-        {index < truncatedText.split("\n").length - 1 && <br />}
-      </React.Fragment>
-    ));
-  };
-
   return (
     <div className="border border-gray-300 p-4 rounded-lg">
       <div className="flex justify-between items-center">
@@ -34,8 +18,7 @@ export default function Article({ post }) {
       </div>
       <div className="text-left mt-4">
         <h2 className="text-2xl font-bold">{post.title}</h2>
-        {formatContent(post.content)}
-        {post.content.replace(/<[^>]*>/g, "").length > 60 && "..."}
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
       </div>
     </div>
   );
