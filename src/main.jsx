a82c6cee107316cb3237253bd3e0e94a8df559c8
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-02 21:28:16 +0900

    とりあえず、記事ページの見た目がわかるように借りのルートを設定

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ArticleDetails from "./components/ArticleDetails.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/posts" element={<ArticleDetails />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
