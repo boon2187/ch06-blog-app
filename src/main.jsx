ecde5b6bf3a71110d37a352ddbd9cc679889f613
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-04 08:13:05 +0900

    ルーティング部分を削除して、単純にAppコンポーネントをレンダリングする元の形に戻した。

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ArticleDetails from "./components/ArticleDetails.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/posts/:id" element={<ArticleDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 );
