d924f8efc1676b562c40b36b84d9936c0afbce71
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-04 08:15:52 +0900

    Headerコンポーネントが共通レイアウトに移動したので削除

--- src/components/ArticleDetails.jsx
+++ src/components/ArticleDetails.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "./Header";
 import { format } from "date-fns";
 import DOMPurify from "dompurify";
 import { useParams } from "react-router-dom";
@@ -11,11 +10,8 @@ export default function ArticleDetails() {
 
   if (!post) {
     return (
-      <div className="w-full h-screen flex flex-col">
-        <Header />
-        <div className="flex flex-col max-w-[800px] mt-12 mx-auto">
-          <p>記事が見つかりませんでした。</p>
-        </div>
+      <div className="flex flex-col max-w-[800px] mt-12 mx-auto">
+        <p>記事が見つかりませんでした。</p>
       </div>
     );
   }
@@ -23,33 +19,30 @@ export default function ArticleDetails() {
   const sanitizedContent = DOMPurify.sanitize(post.content);
 
   return (
-    <div className="w-full h-screen flex flex-col">
-      <Header />
-      <div className="flex flex-col max-w-[800px] mt-12 mx-auto">
-        <img src={post.thumbnailUrl} alt={post.title} />
-        <div className="flex flex-col gap-4">
-          <div className="flex justify-between p-2">
-            <div className="text-gray-500">
-              {format(new Date(post.createdAt), "yyyy/M/d")}
-            </div>
-            <div className="flex gap-2">
-              {post.categories.map((category, index) => (
-                <span
-                  key={index}
-                  className="border border-blue-300 text-blue-500 p-1 rounded-md"
-                >
-                  {category}
-                </span>
-              ))}
-            </div>
+    <div className="flex flex-col max-w-[800px] mt-12 mx-auto">
+      <img src={post.thumbnailUrl} alt={post.title} />
+      <div className="flex flex-col gap-4">
+        <div className="flex justify-between p-2">
+          <div className="text-gray-500">
+            {format(new Date(post.createdAt), "yyyy/M/d")}
           </div>
-          <div className="text-left">
-            <h2 className="text-2xl font-semibold">{post.title}</h2>
-            <p
-              className="mt-6"
-              dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-            />
+          <div className="flex gap-2">
+            {post.categories.map((category, index) => (
+              <span
+                key={index}
+                className="border border-blue-300 text-blue-500 p-1 rounded-md"
+              >
+                {category}
+              </span>
+            ))}
           </div>
+        </div>
+        <div className="text-left">
+          <h2 className="text-2xl font-semibold">{post.title}</h2>
+          <p
+            className="mt-6"
+            dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+          />
         </div>
       </div>
     </div>
