00557820c7ef2222078b4f09b574f1caaee6c1fb
boon2187 <fumiya.mai.ito@gmail.com>
2025-04-09 22:07:44 +0900

    お問い合わせとBlogをLinkコンポーネントでリンク先を追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center px-10 bg-gray-900 h-[72px]">
-      <a className="text-xl text-white">Blog</a>
-      <button className="text-white bg-gray-900 mt-1 cursor-pointer">
+      <Link to="/" className="text-xl text-white">
+        Blog
+      </Link>
+      <Link
+        to="/contact"
+        className="text-white bg-gray-900 mt-1 cursor-pointer"
+      >
         お問い合わせ
-      </button>
+      </Link>
     </header>
   );
 }
